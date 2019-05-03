b79148e8c15f717c9f294fe1e2b4c145043550b7
Cristiana Antalut <cristiana.antalut@gmail.com>
2019-05-03 23:43:45 +0300

    halley done

--- tema6/ConsoleApp/Program.cs
+++ tema6/ConsoleApp/Program.cs
@@ -11,27 +11,32 @@ namespace ConsoleApp
     {
         // POly generator
         // https://solvemymath.com/online_math_calculator/algebra_combinatorics/polynomial_calculator/polynomial_generate_from_roots.php
-        private const string p1 = "x^3 - 53x^2 + 152x^1 - 100x^0"; // roots 1, 2, 50
-        private const string p2 = "x^2 - 13x^1 + 40x^0"; //roots 5, 8
-        private const string p3 = "x^5 - 15x^4 + 85x^3 - 225x^2 + 274x - 120"; //roots 1-5 
-        private const string p4 = "x^6 - 33x^5 + 439x^4 - 3003x^3 + 11080x^2 - 20724x^1 + 15120x^0"; //2, 4, 6, 9, 5, 7
-
+        private const string p1 = "x^3 -53x^2 +152x^1 -100x^0"; // roots 1, 2, 50
+        private const string p2 = "x^2 -13x^1 +40x^0"; //roots 5, 8
+        private const string p3 = "x^5 -15x^4 +85x^3 -225x^2 +274x -120"; //roots 1-5 
+        private const string p4 = "x^6 -33x^5 +439x^4 -3003x^3 +11080x^2 -20724x^1 +15120x^0"; //2, 4, 6, 9, 5, 7
+        private const string p5 = "2x^9 +2x^8 -1x^7 -2x^6 -4x^5 -1x^4 +0x^3 -3x^2 +0x^1 -3";
         static void Main(string[] args)
         {
-            TestParsePoly(p2);
-
+            TestParsePoly(p5);
             Console.ReadKey();
         }
 
         public static void TestParsePoly(string p)
         {
-            Poly poly = new Poly();
+            Poly poly = new Poly(); 
             poly.ParsePoly(p);
             Console.WriteLine("Test ParsePoly");
             foreach (var c in poly.Coefficients)
             {
                 Console.WriteLine(c.Value + " :: x^" + c.Key);
             }
+
+            poly.FindInterval();
+            Console.WriteLine(poly.ComputeHalley());
+            
+            //Test write in file 
+            //poly.WriteSolutionInFile("test1");
         }
     }
 }
--- tema6/T6.Logic/Ploy.cs
+++ tema6/T6.Logic/Ploy.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
+using MathNet.Numerics.LinearAlgebra.Complex;
+using MathNet.Numerics.Random;
 using MathNet.Symbolics;
 
 namespace T6.Logic
@@ -9,29 +12,28 @@ namespace T6.Logic
     public class Poly
     {
         //                       power , coeff
-        private List<KeyValuePair<int, long>> coefficients;
-
-        public List<KeyValuePair<int, long>> Coefficients
+        private Dictionary<int, long> coefficients;
+        public Dictionary<int, long> Coefficients
         {
             get { return coefficients; }
         }
 
-        private List<KeyValuePair<int, long>> roots;
+        private Dictionary<int, long> roots;
         private Tuple<long, long> interval;
         private string poly;
-        private int n;
-        private double epsilon;
+        private int degree;
+        private double epsilon = 1E-8;
 
 
         public Poly()
         {
-            epsilon = ComputeMachinePrecision();
-            coefficients = new List<KeyValuePair<int, long>>();
-
+            //epsilon = ComputeMachinePrecision();
+            coefficients = new Dictionary<int, long>();
         }
 
-        public string[] ParsePoly(string poly)
+        public void ParsePoly(string poly)
         {
+            this.poly = poly;
             var items = poly.Split().ToArray();
             coefficients.Clear();
 
@@ -42,25 +44,95 @@ namespace T6.Logic
                     var subItems = i.Split('x');
                     string power = subItems[1].Split('^')[1] != "" ? subItems[1].Split('^')[1] : "1";
                     string coeff = subItems[0] != "" ? subItems[0] : "1" ;
-                    coefficients.Add(new KeyValuePair<int, long> (int.Parse(power), long.Parse(coeff)));
+                    coefficients.Add(int.Parse(power), long.Parse(coeff));
                 }
             }
-            return items;
+
+            degree = coefficients.Aggregate((l, r) => l.Key > r.Key ? l : r).Key;
         }
 
         public void FindInterval()
         {
+            long A = coefficients.Aggregate((l, r) => l.Value > r.Value ? l : r).Value;
+            long a0 = coefficients.First().Value;
+            long R = (Math.Abs(a0) + A)/Math.Abs(a0);
 
+            interval = Tuple.Create(-R, R);
+            Console.WriteLine(" [" + interval.Item1 + " , " + interval.Item2 + "]");
         }
 
-        public void ComputeHalley()
+        private long GetRandom()
         {
-
+            Random random = new Random();
+            return random.Next((int)interval.Item1, (int)interval.Item2);
         }
 
-        public void ComputeHorner()
+        public long ComputeHalley()
         {
+            long x0 = GetRandom();
+            long x = GetRandom();
+            int k = 1;
+            long fd = ComputeHorner(ComputeDerivative(coefficients), x);
+            long sd = ComputeHorner(ComputeDerivative(ComputeDerivative(coefficients)), x);
+            long xk = ComputeHorner(coefficients, x0);
+            long delta = 0;
+            do
+            {
+                //Console.WriteLine(k);
+                long A = 2 * ((long)Math.Pow(fd, 2)) - xk * sd;
+                if (Math.Abs(A) > epsilon || A != 0)
+                {
+                    delta = (ComputeHorner(coefficients, x) * ComputeHorner(ComputeDerivative(coefficients), x)) / A;
+                    x = x - delta;
+                    k++;
+                }
+                else
+                {
+                    break;
+                }
+            } while (Math.Abs(delta) >= epsilon && k <= 30000 && Math.Abs(delta) <= Math.Pow(10, 8));
 
+            if (Math.Abs(delta) < epsilon)
+                return x;
+            else
+            {
+                return ComputeHalley();
+            }
+        }
+
+        public Dictionary<int, long> ComputeDerivative(Dictionary<int, long> p)
+        {
+            Dictionary<int, long> derivative = new Dictionary<int, long>();
+
+            foreach (var c in p)
+            {
+                if(c.Key != 0)
+                    derivative.Add(c.Key-1, (c.Value * c.Key));
+            }
+
+            //foreach (var d in derivative)
+            //{
+            //    Console.WriteLine(d.Key + " :: " + d.Value);
+            //}
+            return derivative;
+        }
+
+        public long ComputeHorner(Dictionary<int, long> polyDictionary, long x)
+        {
+            long result = 0;
+            foreach (var p in polyDictionary)
+            {
+                result += p.Value * x;
+            }
+
+            //for (int i = 1; i < (p.Count - 1); i++)
+            //{
+            //    result += p[i];
+            //    result *= x;
+            //}
+            //result += p[p.Count - 1];
+
+            return result;
         }
 
         public void ValidateRoots()
@@ -71,13 +143,13 @@ namespace T6.Logic
         public void WriteSolutionInFile(string fileName)
         {
             using (System.IO.StreamWriter file =
-                new System.IO.StreamWriter(@"../../../ resources /" + fileName + ".txt", true))
+                new System.IO.StreamWriter(@"../../../resources /" + fileName + ".txt", true))
             {
                 file.WriteLine("Polinomial: " + poly + "\n");
 
-                foreach (var r in roots)
+                foreach (var c in coefficients)
                 {
-                    file.WriteLine(r);
+                    file.WriteLine(c.Key + " :: " + c.Value);
                 }
             }
         }
